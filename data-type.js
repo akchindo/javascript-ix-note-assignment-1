8719e8a9b58c368dc10ead8c2013b5005c4224a3
akchindo <abdullahichindo16@gmail.com>
2021-05-18 15:07:15 +0100

    initial commit

---
+++ data-type.js
@@ -0,0 +1,27 @@
+var a; 
+var b;
+var c;
+
+// strings
+a = "Chindo"
+console.log (a)
+console.log (typeof(a))
+
+
+// booleans
+a = 5;
+b = 3;
+c = a > b
+console.log (c)
+console.log (typeof(c))
+
+
+// integers
+a = 19
+console.log (a)
+console.log (typeof(a))
+
+// floats
+a = 5.000000000000
+console.log ( (a).toFixed(8) )
+console.log (typeof(a))
